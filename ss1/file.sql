00adaa24aa64496a30b4f4ff00e8cdb443095bf6
unknown <ttn.hdttb152004@gmail.com>
2023-01-10 10:55:27 +0700

    new file:   ss1/bai_tap/file.sql

---
+++ ss1/file.sql
@@ -0,0 +1,12 @@
+create schema student_management;
+use student_management;
+create table class(
+id int,
+name varchar(30)
+);
+create table teacher(
+id int,
+name varchar(30),
+age int,
+country varchar(30)
+);
