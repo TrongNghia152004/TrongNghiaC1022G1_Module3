d5ce9898af70da6bea0b57c3cef1d2086eea2f05
unknown <ttn.hdttb152004@gmail.com>
2023-01-10 10:58:50 +0700

    update

--- module3/ss1/bai_tap/file.sql
+++ module3/ss1/bai_tap/file.sql
@@ -2,11 +2,13 @@ create schema student_management;
 use student_management;
 create table class(
 id int,
-name varchar(30)
+name varchar(30),
+primary key (id)
 );
 create table teacher(
 id int,
 name varchar(30),
 age int,
-country varchar(30)
+country varchar(30),
+primary key (id)
 );
